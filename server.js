c7ed8d384f35d5a52edf10d069a78427f7fda9dc
Andrés Méndez <andrex873@gmail.com>
2014-05-12 15:10:31 -0500

    Se configura el server como modulo y se crea archivo index.js

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var http = require('http');
 
-// Definir la funcion que se envia al servidor
-var onRequest = function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("hola Andres..");
-    response.end();
+function iniciar() {
+    // Definir la funcion que se envia al servidor
+    var onRequest = function(request, response) {
+        console.log("Petición recibida.");
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write("hola Andres..");
+        response.end();
+    }
+    // Crear el servidor
+    http.createServer(onRequest).listen(8888);    
+    console.log("Server corriendo en http://localhost:8888/");
 }
-// Crear el servidor
-http.createServer(onRequest).listen(8888);
 
-console.log("Server corriendo en http://localhost:8888/");
+exports.iniciar = iniciar;
